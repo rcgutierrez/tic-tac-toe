683de4c2c83d8b46d450f98b0b0c2c4332a4ac83
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-22 14:46:26 -0500

    first dive into TTT

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-// wait for the DOM to finish loading
-$(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+// // wait for the DOM to finish loading
+// $(document).ready(function() {
+//   // all code to manipulate the DOM
+//   // goes inside this function
+// });
 
-});
+//declare object with array of {box: numberedBox, taken: false} 
+
+//declare variable assinging querySelectorAll('box')
+//forEach every box item
+//
