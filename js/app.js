2e2072655cb6af99575200b63775004d8a537c09
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-22 15:47:23 -0500

    psuedo code update

--- js/app.js
+++ js/app.js
@@ -5,14 +5,23 @@
 // });
 
 // declare boonlean variable activePlayer; where true is for 'X' and false is for "O"
+var activePlayer;
 
-//declare firstPlayer function
-//
+//declare startButton function
+function startButton(){
+  //add event listener to .button in header
+  //asks for which character firstPlayer wants to be, only takes X or O
+  //if X=> activePlayer = true, if O => activePlayer = false
+}
 
 //declare positionsTaken object
 //forEach (.board .row) to fill object with array of {box: numberedBox, taken: false}
 
-//declare variable assinging querySelectorAll('box')
+
+//declare variable boxClicked assinging querySelectorAll('box')
 //forEach every box item, create event listener (function){
+//searches the positionsTaken array for index of 
+
+
 //
 //
