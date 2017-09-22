ce1b6026b2af8e1d0669b8b78008b7ebc83960b4
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-22 15:23:14 -0500

    psuedo code update

--- js/app.js
+++ js/app.js
@@ -4,8 +4,15 @@
 //   // goes inside this function
 // });
 
-//declare object with array of {box: numberedBox, taken: false} 
+// declare boonlean variable activePlayer; where true is for 'X' and false is for "O"
+
+//declare firstPlayer function
+//
+
+//declare positionsTaken object
+//forEach (.board .row) to fill object with array of {box: numberedBox, taken: false}
 
 //declare variable assinging querySelectorAll('box')
-//forEach every box item
+//forEach every box item, create event listener (function){
+//
 //
