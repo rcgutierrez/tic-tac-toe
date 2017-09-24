6e809a63f307f2f99c8d0de045ba0cc6c209f1f2
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-24 15:42:49 -0500

    boxClicked, sortAndCheck methods added

--- js/app.js
+++ js/app.js
@@ -5,23 +5,139 @@
 // });
 
 // declare boonlean variable activePlayer; where true is for 'X' and false is for "O"
-var activePlayer;
-
-//declare startButton function
-function startButton(){
-  //add event listener to .button in header
-  //asks for which character firstPlayer wants to be, only takes X or O
-  //if X=> activePlayer = true, if O => activePlayer = false
-}
-
-//declare positionsTaken object
-//forEach (.board .row) to fill object with array of {box: numberedBox, taken: false}
-
-
-//declare variable boxClicked assinging querySelectorAll('box')
-//forEach every box item, create event listener (function){
-//searches the positionsTaken array for index of 
-
-
+var activePlayer = true;
+var winningCombos = [
+                      ['1','2','3'],
+                      ['4','5','6'],
+                      ['7','8','9'],
+                      ['1','5','9'],
+                      ['3','5','7'],
+                      ['1','4','7'],
+                      ['2','5','8'],
+                      ['3','6','9']
+                    ];
+var xChosen = [[], [], [], [], [], [], [], []];
+var oChosen = [[], [], [], [], [], [], [], []];
+var boxNumber = 1;
+//declare startButton function NEEDS A LOOK
+// function startButton(){
+//   //add event listener to .button in header
+//   var sButton = document.querySelector('#start');
+//   // sButton.addEventListener('click', function (){
+//     //asks for which character firstPlayer wants to be, only takes X or O
+//     console.log(sButton);
+//   // })
 //
-//
+//   console.log(sButton);
+//   //if X=> activePlayer = true, if O => activePlayer = false
+// };
+// startButton();
+
+
+
+function sortAndCheck(arr, number){
+  switch(number){
+    case '1':
+      arr[0].push('1');
+      arr[3].push('1');
+      arr[5].push('1');
+      // console.log(arr);
+      break;
+    case '2':
+      arr[0].push('2');
+      arr[6].push('2');
+      // console.log(arr);
+      break;
+    case '3':
+      arr[0].push('3');
+      arr[4].push('3');
+      arr[7].push('3');
+      // console.log(arr);
+      break;
+    case '4':
+      arr[1].push('4');
+      arr[5].push('4');
+      // console.log(arr);
+      break;
+    case '5':
+      arr[1].push('5');
+      arr[3].push('5');
+      arr[4].push('5');
+      arr[6].push('5');
+      // console.log(arr);
+      break;
+    case '6':
+      arr[1].push('6');
+      arr[7].push('6');
+      // console.log(arr);
+      break;
+    case '7':
+      arr[2].push('7');
+      arr[4].push('7');
+      arr[5].push('7');
+      // console.log(arr);
+      break;
+    case '8':
+      arr[2].push('8');
+      arr[6].push('8');
+      // console.log(arr);
+      break;
+    case '9':
+      arr[2].push('9');
+      arr[3].push('9');
+      arr[7].push('9');
+      // console.log(arr)
+      break;
+  }
+  for(var i=0; i<arr.length; i++){
+    arr[i].sort();
+  };
+  for(var i=0; i<winningCombos.length; i++){
+    var winningNum = winningCombos[i].join('');
+    for(var p=0; p<arr.length; p++){
+      var playerCombos = arr[p].join('');
+      if(playerCombos === winningNum){
+        alert("You're a winner, baby");
+        console.log(playerCombos + ' ' + winningNum);
+        winningCombos.splice(i);
+        return true
+      }
+    }
+  };
+  return false;
+};
+
+
+var boxClicked = document.querySelectorAll('.row .box');
+boxClicked.forEach(function (thisBox){
+  thisBox.setAttribute('boxId', boxNumber);
+  boxNumber++;
+});
+
+boxClicked.forEach(function (thisBox){
+
+    thisBox.addEventListener('click', function clicking(){
+      if(activePlayer === true){
+        thisBox.innerText = 'X';
+        activePlayer = false;
+        if(sortAndCheck(xChosen, thisBox.getAttribute('boxId')) == true){
+          removeClickability(clicking);
+        };
+        thisBox.removeEventListener('click', clicking);
+      }else{
+        thisBox.innerText = 'O';
+        activePlayer = true;
+        sortAndCheck(xChosen, thisBox.getAttribute('boxId'))
+        if(sortAndCheck(xChosen, thisBox.getAttribute('boxId')) == true){
+          removeClickability(clicking);
+        };
+        thisBox.removeEventListener('click', clicking);
+      };
+    })
+  });
+
+function removeClickability(removedFunction){
+  boxClicked.forEach(function (thisBox){
+    thisBox.removeEventListener('click', removedFunction);
+  });
+};
